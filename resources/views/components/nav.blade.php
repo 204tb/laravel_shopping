aa32b5f1a39bf7e4512caecaca9a987cf466b181
204tb <200204tb@yse-c.net>
2022-01-28 13:59:27 +0900

    ナビゲーションバーの追加

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,4 @@
+<nav class="nav bg-light">
+    <a href="{{route('admin.item.index')}}" class="nav-link">商品一覧</a>
+    <a href="{{route('admin.item.create')}}" class="nav-link">商品入力</a>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--BootStrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.tailwindcss.com"></script>
+
     <title>Document</title>
 </head>
 
-<body class="container">
-<nav class="nav bg-light">
-    <a href="{{route('admin.item.index')}}" class="nav-link">商品一覧</a>
-    <a href="{{route('admin.item.create')}}" class="nav-link">商品入力</a>
-</nav>
-
+<body>
+@include("components.nav")
     <div class="container">
         @yield("content")
     </div>
-    
+
 </body>
+
 </html>
